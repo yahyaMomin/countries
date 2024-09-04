2ac02642f4f3fa147e10782fcd2964ed73bed1ff
yahya momin <yahyamomin06@gmail.com>
2024-09-05 02:52:31 +0530

    fix bugs

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,11 +4,11 @@ import { Link } from "react-router-dom";
 const HomePage = () => {
    return (
       <div>
-         <div className="flex justify-center flex-col h-[100dvh]">
+         <div className="flex justify-center flex-col items-center h-[100dvh]">
             <h1 className="text-2xl mb-2">get all info in one place</h1>
             <Link
-               to="/home"
-               className="bg-primary px-10 py-2 text-lg rounded-sm text-black"
+               to="/main"
+               className="bg-primary px-10 py-2 text-lg rounded-sm w-fit text-black"
             >
                explore
             </Link>
--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const PageNotFound = () => {
-   return <div>PageNotFound</div>;
+   return (
+      <div>
+         <div className="flex justify-center flex-col items-center h-[100dvh]">
+            <h1 className="text-2xl mb-2">page not found :( </h1>
+            <Link
+               to="/main"
+               className="bg-primary px-10 py-2 text-lg rounded-sm w-fit text-black"
+            >
+               go to main page
+            </Link>
+         </div>
+      </div>
+   );
 };
 
 export default PageNotFound;
