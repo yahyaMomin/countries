86e82ee239c3c6743dedf3e7a4562580b63eb5aa
yahya momin <yahyamomin06@gmail.com>
2024-09-05 02:44:53 +0530

    add vercel.json

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
-   return <div>HomePage</div>;
+   return (
+      <div>
+         <div className="flex justify-center flex-col h-[100dvh]">
+            <h1 className="text-2xl mb-2">get all info in one place</h1>
+            <Link
+               to="/home"
+               className="bg-primary px-10 py-2 text-lg rounded-sm text-black"
+            >
+               explore
+            </Link>
+         </div>
+      </div>
+   );
 };
 
 export default HomePage;
